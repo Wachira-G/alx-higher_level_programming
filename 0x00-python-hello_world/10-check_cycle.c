979f5c84e681ed5ed7805633e7a5a078c9c5f0a9
Gabriel Wachira <gabbyk.com@gmail.com>
2023-06-06 00:57:15 +0300

    Add task 10 files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks a linked list for a cycle
+ * @list: pointer to the head of the list to check for cycle
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *head;
+	listint_t *current;
+
+	head = list;
+	current = head;
+	while (head != NULL)
+	{
+		head = head->next;
+		if (head == current)
+			return (1);
+	}
+	return (0);
+}
