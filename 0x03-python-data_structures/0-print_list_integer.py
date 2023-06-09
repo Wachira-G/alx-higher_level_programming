2063af9c589857121f918a2852cc0ad100e35f07
Gabriel Wachira <gabbyk.com@gmail.com>
2023-06-09 07:52:42 +0300

    Add task 1 files: 1-element_at.py, add doc in 0-print

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    ''' Prints all integers of a list,
+        assume that the list only contains integers'''
     for item in my_list:
         print('{}'.format(item))
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """Retrieves an element from a list like in C """
+    size = len(my_list)
+    if idx < 0:
+        return None
+    elif idx > size - 1:   # len starts from 1
+        return None
+    else:
+        return my_list[idx]
