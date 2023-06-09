ff29649b02dd9d8728dd47abdf0a2f1a5362e4ed
Gabriel Wachira <gabbyk.com@gmail.com>
2023-06-09 11:15:42 +0300

    Add task 8 files: 8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    '''Returns a tuple with the length of a string and its first character.
+        If sentence is empty, the first character should be equal to None'''
+    sentence_len = len(sentence)
+    if sentence == 0:
+        char = None
+    else:
+        char = sentence[0]
+    return sentence_len, char
