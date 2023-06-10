3365ad6b07c2d3f971963ac07a7f2505ddfebf3d
Gabriel Wachira <gabbyk.com@gmail.com>
2023-06-10 22:24:33 +0300

    fix ommission sentence"_len": 8-multiple_return.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+s#!/usr/bin/python3
 def multiple_returns(sentence):
     '''Returns a tuple with the length of a string and its first character.
         If sentence is empty, the first character should be equal to None'''
     sentence_len = len(sentence)
-    if sentence == 0:
+    if sentence_len == 0:
         char = None
     else:
         char = sentence[0]
