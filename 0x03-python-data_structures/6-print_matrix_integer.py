4845b23bf17271beed6438c46c4021e26ad184d7
Gabriel Wachira <gabbyk.com@gmail.com>
2023-06-10 22:18:59 +0300

    fix cast: 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     '''Prints a matrix of integers.'''
     for row in matrix:
-        row_format = ' '.join(['{}'] * len(row))  # == '{} {}' if len = 2
+        row_format = ' '.join(['{:d}'] * len(row))  # '{} {}' if len=2
         print(row_format.format(*row))  # the '*' is sequence unpacking
