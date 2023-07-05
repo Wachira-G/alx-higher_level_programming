8d4356dbdbf49d7f92843c3d20ef7d7a3dd0f2a5
Gabriel Wachira <gabbyk.com@gmail.com>
2023-07-05 18:16:27 +0300

    Add case for no argument

---
+++ 0x07-python-test_driven_development/5-text_indentation_3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Doc """
+
+def text_indentation(text=""):
+    """ Doc """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    after_new_line = False
+    for c in text:
+        if after_new_line:
+            if c == " ":
+                continue
+            after_new_line = False
+        if c == '.' or c == '?' or c == ':':
+            print(c)
+            print("")
+            after_new_line = True
+        else:
+            print(c, end="")
