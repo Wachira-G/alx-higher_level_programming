1107b71a667ebdce3f6a37d0db4c1dff93abd25a
Gabriel Wachira <gabbyk.com@gmail.com>
2023-07-11 00:04:19 +0300

    Add task 5 files

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+''' A module implementing a simple class
+'''
+
+
+class BaseGeometry:
+    """a simple class
+    """
+    def area(self):
+        """a simple area method
+
+        Raises:
+            Exception: does just that
+        """
+        raise Exception("area() is not implemented")
