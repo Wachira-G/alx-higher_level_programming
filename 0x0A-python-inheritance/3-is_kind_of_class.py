5920b9503b51ffde8f8ebc63958bbfeabe747fff
Gabriel Wachira <gabbyk.com@gmail.com>
2023-07-10 23:47:48 +0300

    Add task 3 files

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""A module that demonstrates inheritance
+"""
+
+
+def is_kind_of_class(obj, a_class)->bool:
+    """a funtion that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+     the specified class ; otherwise False."
+
+    Args:
+        obj (Any): the object
+        a_class (Any): the class
+
+    Returns:
+        bool: true of false
+    """    
+    return isinstance(obj, a_class)
