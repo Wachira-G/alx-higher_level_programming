6c9789083fd18b1c3e8d41da401ae4b26114fa1c
Gabriel Wachira <gabbyk.com@gmail.com>
2023-07-11 23:39:17 +0300

    Add no new line at end

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,7 +8,7 @@ def read_file(filename=""):
     """A function that reads a file and prints it to the stdout
 
     Args:
-        filename (str, optional): the file wer are reading. Defaults to ""
+        filename (str, optional): the file we are reading. Defaults to ""
     """
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end='')
