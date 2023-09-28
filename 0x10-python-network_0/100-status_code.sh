b933ebaa835a76f8d1b0dc0b122cb7aa59416c50
Gabriel Wachira <gabbyk.com@gmail.com>
2023-09-29 00:57:10 +0300

    fix shebang

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response. not to use any pipe, redirection, etc, nor use ; and &&
 curl -s -o /dev/null "$1" -w "%{http_code}"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response. Your script must send a POST request with the contents of a file, passed with the filename as the second argument of the script, in the body of the request
 curl -X POST -H "Content-Type: application/json" -d "$2" "$1"
