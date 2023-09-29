429d23b048a3fa80d894543fa566f52ab7d7712d
Gabriel Wachira <gabbyk.com@gmail.com>
2023-09-29 07:25:37 +0300

    Update files: fix typos, ommission

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -s --head -X GEI "$1" | grep -i "Allow:" | awk '{print substr($0, index($0, $2))}'
+curl -s --head -X GET "$1" | grep -i "Allow:" | awk '{print substr($0, index($0, $2))}'
