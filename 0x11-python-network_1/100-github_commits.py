621bc46377de432231de55d9bfd9dddab557d66c
Gabriel Wachira <gabbyk.com@gmail.com>
2023-09-29 21:14:44 +0300

    Add task 10 file

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
 """ This module... """
 if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    repo = argv[1]
+    username = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+            username,
+            repo
+    )
+    with requests.get(url) as response:
+        commits = response.json()
+        count = 10
+        for commit in commits:
+            if count < 1:
+                break
+            sha = commit['sha']
+            author = commit['commit']['author']['name']
+            # date = commit['commit']['author']['date']
+            count -= 1
+            print('{}: {}'.format(sha, author)) # , date)
